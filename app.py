78a30c5e7b431452e685f5a2106966f218750574
TSAR46 <134359957+TSAR46@users.noreply.github.com>
2026-03-01 15:31:50 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, join_room
 import random
-import eventlet
-
-eventlet.monkey_patch()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
+app.config["SECRET_KEY"] = "secret!"
 
-socketio = SocketIO(app, async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 waiting_player = None
 games = {}
+
 
 def generate_secret():
     digits = []
@@ -19,7 +17,8 @@ def generate_secret():
         num = str(random.randint(0, 9))
         if num not in digits:
             digits.append(num)
-    return ''.join(digits)
+    return "".join(digits)
+
 
 def count_matches(secret, guess):
     secret_list = list(secret)
@@ -30,11 +29,13 @@ def count_matches(secret, guess):
             secret_list.remove(digit)
     return count
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@socketio.on('connect')
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@socketio.on("connect")
 def handle_connect():
     global waiting_player
 
@@ -51,23 +52,28 @@ def handle_connect():
             "secret": secret,
             "players": [waiting_player, request.sid],
             "current_turn": waiting_player,
-            "active": True
+            "active": True,
         }
 
         socketio.server.enter_room(waiting_player, game_id)
         join_room(game_id)
 
-        socketio.emit("startGame", {
-            "player1": waiting_player,
-            "player2": request.sid,
-            "currentTurn": waiting_player
-        }, room=game_id)
+        socketio.emit(
+            "startGame",
+            {
+                "player1": waiting_player,
+                "player2": request.sid,
+                "currentTurn": waiting_player,
+            },
+            room=game_id,
+        )
 
         print("Game started:", game_id, "Secret:", secret)
 
         waiting_player = None
 
-@socketio.on('makeGuess')
+
+@socketio.on("makeGuess")
 def handle_guess(data):
     guess = data.get("guess")
 
@@ -88,43 +94,46 @@ def handle_guess(data):
     if not game["active"]:
         return
 
-    # Check turn
     if game["current_turn"] != request.sid:
         return
 
     correct_count = count_matches(game["secret"], guess)
 
-    socketio.emit("guessResult", {
-        "player": request.sid,
-        "guess": guess,
-        "correctCount": correct_count
-    }, room=game_id)
+    socketio.emit(
+        "guessResult",
+        {
+            "player": request.sid,
+            "guess": guess,
+            "correctCount": correct_count,
+        },
+        room=game_id,
+    )
 
-    # Win condition
     if correct_count == 4:
         game["active"] = False
-        socketio.emit("gameOver", {
-            "winner": request.sid
-        }, room=game_id)
+        socketio.emit("gameOver", {"winner": request.sid}, room=game_id)
         return
 
-    # Switch turn
     players = game["players"]
-    game["current_turn"] = players[0] if request.sid == players[1] else players[1]
+    game["current_turn"] = (
+        players[0] if request.sid == players[1] else players[1]
+    )
 
-    socketio.emit("turnChanged", {
-        "currentTurn": game["current_turn"]
-    }, room=game_id)
+    socketio.emit(
+        "turnChanged",
+        {"currentTurn": game["current_turn"]},
+        room=game_id,
+    )
 
-@socketio.on('disconnect')
+
+@socketio.on("disconnect")
 def handle_disconnect():
     global waiting_player
     print("Disconnected:", request.sid)
+
     if waiting_player == request.sid:
         waiting_player = None
 
-import os
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    socketio.run(app, host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    socketio.run(app)
